c1b71da53e3e173424a5a625c3e8d990e92d7e3a
kolawole akinmosin <kolawoleakinmosin01@gmail.com>
2023-03-07 15:15:17 +0000

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new', as: :new_article
+  get '/articles/:id', to: 'articles#show', as: :article
+  post '/articles', to: 'articles#create'
+
 end
